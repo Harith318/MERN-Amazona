def0d665c86e16198ea7653d04fd2c01f51e0ef7
Hairth <96919510+Harith318@users.noreply.github.com>
2023-01-05 16:00:15 +0300

    L 21

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'omar',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'as',
+      email: 'as@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       //_id: '1',
